5d6432e692e2bf9ff60d85bf60a90bd7a091c762
MR1meteor <82905956+MR1meteor@users.noreply.github.com>
2023-04-20 18:35:08 +0300

    Edit Self-crossing area validation

--- ApiContestNew.Core/Models/Entities/Area.cs
+++ ApiContestNew.Core/Models/Entities/Area.cs
@@ -40,15 +40,30 @@ namespace ApiContestNew.Core.Models.Entities
                 return false;
             }
 
-            for (int i = 0; i < AreaPoints.Count - 3; i++)
+            for (int i = 0; i < AreaPoints.Count - 1; i++)
             {
-                for (int j = i + 2; j < AreaPoints.Count - 1; j++)
+                for (int k = 3; k < AreaPoints.Count; k++)
                 {
+                    int j = i + 2;
+                    int j1 = j;
+                    int j2 = j + 1;
+
+                    if (j2 >= AreaPoints.Count)
+                    {
+                        j2 = 0;
+                    }
+
+                    if (j1 >= AreaPoints.Count)
+                    {
+                        j2 = 1;
+                        j1 = 0;
+                    }
+
                     if (AreLinesCrossing(
                         ((List<LocationPoint>)AreaPoints)[i],
                         ((List<LocationPoint>)AreaPoints)[i + 1],
-                        ((List<LocationPoint>)AreaPoints)[j],
-                        ((List<LocationPoint>)AreaPoints)[j + 1]))
+                        ((List<LocationPoint>)AreaPoints)[j1],
+                        ((List<LocationPoint>)AreaPoints)[j2]))
                     {
                         return false;
                     }
@@ -80,19 +95,27 @@ namespace ApiContestNew.Core.Models.Entities
 
         private bool AreLinesCrossing(LocationPoint p1, LocationPoint p2, LocationPoint p3, LocationPoint p4) // TODO: Redo this (doesnt work)
         {
-            double equationIndex1 = (p1.Longitude - p2.Longitude) / (p1.Latitude - p2.Latitude);
-            double equationOffset1 = p2.Longitude - equationIndex1 * p2.Latitude;
+            double equationIndex1 = (p2.Longitude - p1.Longitude) / (p2.Latitude - p1.Latitude);
+            double equationOffset1 = (p2.Latitude * p1.Longitude - p1.Latitude * p2.Longitude) / (p2.Latitude - p1.Latitude);
 
-            double equationIndex2 = (p3.Longitude - p4.Longitude) / (p3.Latitude - p4.Latitude);
-            double equationOffset2 = p4.Longitude - equationIndex2 * p4.Latitude;
+            double equationIndex2 = (p4.Longitude - p3.Longitude) / (p4.Latitude - p3.Latitude);
+            double equationOffset2 = (p4.Latitude * p3.Longitude - p3.Latitude * p4.Longitude) / (p4.Latitude - p3.Latitude);
 
             try
             {
                 var crossingLongitude = equationIndex1 * (equationOffset2 - equationOffset1) / (equationIndex1 - equationIndex2) + equationOffset1;
                 var crossingLatitude = (equationOffset2 - equationOffset1) / (equationIndex1 - equationIndex2);
 
-                if (LocationPoint.IsPointOnLine(new LocationPoint { Latitude = crossingLatitude, Longitude = crossingLongitude } ,p1, p2) &&
-                    LocationPoint.IsPointOnLine(new LocationPoint { Latitude = crossingLatitude, Longitude = crossingLongitude }, p3, p4))
+                if (
+                    (p1.Latitude < crossingLatitude && p2.Latitude > crossingLatitude ||
+                    p1.Latitude > crossingLatitude && p2.Latitude < crossingLatitude) &&
+                    (p1.Longitude < crossingLongitude && p2.Longitude > crossingLongitude ||
+                    p1.Longitude > crossingLongitude && p2.Longitude < crossingLongitude) &&
+                    (p3.Latitude > crossingLatitude && p4.Latitude < crossingLatitude ||
+                    p3.Latitude < crossingLatitude && p4.Latitude > crossingLatitude) &&
+                    (p3.Longitude < crossingLongitude && p4.Longitude > crossingLongitude ||
+                    p3.Longitude > crossingLongitude && p4.Longitude < crossingLongitude)
+                    )
                 {
                     return true;
                 }
