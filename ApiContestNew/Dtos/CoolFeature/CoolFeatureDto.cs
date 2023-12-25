b00400a115d27da89c8ed9ad401b0cde86fc42b1
Kiruha <smau6@ya.ru>
2023-12-25 10:17:09 +0300

    Add cool feature

---
+++ ApiContestNew/Dtos/CoolFeature/CoolFeatureDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiContestNew.Dtos.CoolFeature
+{
+    public class CoolFeatureDto
+    {
+        public string CoolField { get; set; } = string.Empty;
+        public string MegaCoolField { get; set; } = string.Empty;
+    }
+}
