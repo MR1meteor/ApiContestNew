65b6fa313b73ce16736d2e88ab85636d554a72d1
Kiruha57 <154590472+Kiruha57@users.noreply.github.com>
2023-12-25 10:21:37 +0300

    Add cool feature (#1)
    Co-authored-by: Kiruha <smau6@ya.ru>

---
+++ ApiContestNew/Controllers/CoolController.cs
@@ -0,0 +1,22 @@
+﻿using ApiContestNew.Dtos.CoolFeature;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiContestNew.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoolController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCoolResult(CoolFeatureDto dto)
+        {
+            if (String.IsNullOrWhiteSpace(dto.CoolField) || String.IsNullOrWhiteSpace(dto.MegaCoolField))
+            {
+                return BadRequest("Not cool");
+            }
+
+            return Ok("Toooooooo coooooool");
+        }
+    }
+}
